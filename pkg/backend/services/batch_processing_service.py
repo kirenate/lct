d181407e64450210f0b97c793630d8c1a25ac2e7
kirenate <ekirenn@gmail.com>
2025-09-24 19:17:47 +0300

    jpg and tiff now converted to png

--- pkg/backend/services/batch_processing_service.py
+++ pkg/backend/services/batch_processing_service.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 import uuid
 import io
+
+from sqlalchemy.dialects.postgresql.pg_catalog import format_type
+
 from repositories.pg_repository import PgRepository
 from repositories.redis_repository import RedisRepository
 from repositories.minio_repository import MinioRepository
+from pdf2image import convert_from_bytes
 import pymupdf
 from PIL  import Image
 
@@ -21,13 +25,13 @@ class Service:
     async def save_document(self, batch: bytes, filename: str) ->  uuid.UUID :
         uid = uuid.uuid4()
         ext = filename.split(".")[-1]
-        if ext == "pdf":
-            doc = pymupdf.open(batch)
-            page = doc.load_page(0)
-            pixmap = page.get_pixmap(dpi=300)
-            img = pixmap.tobytes()
-            self.minio.client.upload_fileobj(io.BytesIO(img), self.minio.bucket_name, str(uid) + ".png")
-            return uid
+        if ext not in [ "png", "jpg", "jpeg", "tiff"]:
+            raise Exception("Invalid file extension")
+        # if ext == "pdf":
+        #     img = io.BytesIO()
+        #     convert_from_bytes(batch)[0].save(fp=img, format="png")
+        #     self.minio.client.upload_fileobj(img, self.minio.bucket_name, str(uid) + ".png")
+        #     return uid
 
         self.minio.client.upload_fileobj(io.BytesIO(batch), self.minio.bucket_name, str(uid)+".png")
         return uid
